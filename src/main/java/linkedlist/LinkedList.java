eb980e74161eb9e5be137bd2c982005ff81225e5
Evan Slaton <evanslaton@gmail.com>
2018-12-03 09:17:01 -0800

    Fixed file structure again

---
+++ src/main/java/linkedlist/LinkedList.java
@@ -0,0 +1,10 @@
+package linkedlist;
+
+public class LinkedList {
+    public Node head;
+
+    LinkedList(Node head) {
+        this.head = head;
+    }
+
+}
---
+++ src/main/java/linkedlist/Node.java
@@ -0,0 +1,9 @@
+package linkedlist;
+
+public class Node {
+    public Node next;
+
+    Node(Node next) {
+        this.next = next;
+    }
+}
