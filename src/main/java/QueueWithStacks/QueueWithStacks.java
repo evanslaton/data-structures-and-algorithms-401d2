f01f27296afde2d9b7a4c4f2d663ce8ee654be8a
Evan Slaton <evanslaton@gmail.com>
2018-12-10 18:40:31 -0800

    finished methods

--- src/main/java/QueueWithStacks/QueueWithStacks.java
+++ src/main/java/QueueWithStacks/QueueWithStacks.java
@@ -2,7 +2,7 @@ package QueueWithStacks;
 
 import StacksAndQueues.Stack;
 
-public class QueueWithStacks<T> {
+public class QueueWithStacks<T>  implements {
     Stack<T> firstStack;
     Stack<T> secondStack;
 
@@ -10,6 +10,25 @@ public class QueueWithStacks<T> {
     public QueueWithStacks(){
         this.firstStack = new Stack();
         this.secondStack = new Stack();
+    }
+
+    //
+    public void enqueue(T value) {
+        if (firstStack.peek() == null) {
+            firstStack.push(value);
+        } else {
+            while (firstStack.peek() != null) {
+                secondStack.push(firstStack.pop());
+            }
+            firstStack.push(value);
+            while (secondStack.peek() != null) {
+                firstStack.push(secondStack.pop());
+            }
+        }
+    }
+
+    public T dequeue() {
+        return firstStack.pop();
     }
 
 }
