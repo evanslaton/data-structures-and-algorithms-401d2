cf85b8138627c379dc5568e5995cc32acd2cc63a
Evan Slaton <evanslaton@gmail.com>
2018-12-17 21:28:05 -0800

    Finished code challelenge

--- src/main/java/fizzbuzztree/FizzBuzzTree.java
+++ src/main/java/fizzbuzztree/FizzBuzzTree.java
@@ -4,20 +4,7 @@ import tree.BinaryTree;
 import tree.Node;
 
 public class FizzBuzzTree {
-    public static void main(String[] args) {
-        BinaryTree testBinaryTree = new BinaryTree();
-
-        testBinaryTree.root = new Node(3, null, null);
-        testBinaryTree.root.left = new Node(1, null, null);
-        testBinaryTree.root.right = new Node(15, null, null);
-        testBinaryTree.root.right.left = new Node (6, null, null);
-        testBinaryTree.root.right.right = new Node (13, null, null);
-
-        Node[] values = testBinaryTree.postOrder(testBinaryTree.root);
-        FizzBuzzTree.fizzBuzzTree(testBinaryTree);
-        Node[] values2 = testBinaryTree.postOrder(testBinaryTree.root);
-    }
-
+    // Takes in a BinaryTree and turns all node values that are divisible by 3 and 5 to 'FizzBuzz', divisible by 5 to 'Buzz' and divisible by 3 to 'Fizz'
     public static BinaryTree<Node> fizzBuzzTree(BinaryTree<Node> tree) {
         if (tree.root == null) {
             return tree;
@@ -26,6 +13,7 @@ public class FizzBuzzTree {
         return tree;
     }
 
+    // Checks each node in the BinaryTree and passes them into the fizzBuzzify helper function
     public static Node recursiveSearch(Node node) {
         if (node == null) {
             return null;
@@ -38,6 +26,7 @@ public class FizzBuzzTree {
         return node;
     }
 
+    // Turns all node values that are divisible by 3 and 5 to 'FizzBuzz', divisible by 5 to 'Buzz' and divisible by 3 to 'Fizz'
     public static void fizzBuzzify(Node node) {
         if ((int) node.value % 3 == 0 && (int) node.value % 5 == 0) {
             node.value = "FizzBuzz";
